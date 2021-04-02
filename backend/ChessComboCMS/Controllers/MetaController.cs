e7a4c8638b049890dc3941ec0dcc384aaf2ee8e7
milosponj <milosponj@yahoo.com>
2021-04-02 10:24:19 +0200

    Add meta controller.

---
+++ backend/ChessComboCMS/Controllers/MetaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessComboCMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MetaController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public MetaController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public ActionResult GetMeta()
+        {
+            return Ok(_config.GetConnectionString("DefaultConnection").Substring(0,9));
+        }
+    }
+}
