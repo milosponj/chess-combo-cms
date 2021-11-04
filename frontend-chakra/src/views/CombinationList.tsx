2a228aa9e22866b97d91151803b8662631e21c2c
natasharp <natasa_sladojevic@yahoo.com>
2021-11-04 18:22:18 +0100

    Clean up unnecessary code

--- frontend-chakra/src/views/CombinationList.tsx
+++ frontend-chakra/src/views/CombinationList.tsx
@@ -7,39 +7,15 @@ import {
   theme,
   Button,
 } from "@chakra-ui/react";
-import axios from "axios";
-import { apiBaseUrl } from "../constants";
-import { setCombo, useStateValue } from "../state";
-import { Combination, Game } from "../interfaces";
-
 
 export const CombinationList = () => {
-  const [, dispatch] = useStateValue();
-  const fetchComboAndGame = async (id: number) => {
-    console.log("editing specific combo");
-    try {
-      const { data: combinationFromApi } = await axios.get<Combination>(
-        `${apiBaseUrl}/combinations/${id}`
-      );
-      const gameId = combinationFromApi.gameId;
-      const { data: gameFromApi } = await axios.get<Game>(
-        `${apiBaseUrl}/games/${gameId}`
-      );
-      dispatch(setCombo(combinationFromApi, gameFromApi));
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <VStack spacing={8}>
             <Button>Add new combination</Button>
-            <Button onChange={() => fetchComboAndGame(10)}>
-              Edit combination
-            </Button>
+            <Button>Edit combination</Button>
           </VStack>
         </Grid>
       </Box>
